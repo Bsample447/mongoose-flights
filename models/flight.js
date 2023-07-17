2b4fbd6603bf07d6688aa14fb2eb0d6b12ead3fa
Bsample447 <brandonsample447@gmail.com>
2023-07-17 15:07:55 -0400

    style update

--- models/flight.js
+++ models/flight.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-  airline: { type: String, enum: ["American", "Southwest", "United"] },
+  airline: { type: String, enum: ["American", "Southwest", "United", "Delta"] },
   airport: {
     type: String,
-    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN", "OAJ", "RDU"],
     default: "DEN",
   },
   flightNo: { type: Number, require: true, min: 10, max: 9999 },
